b3e0d542df2d2b697e7dac7f4f52c6da8e2203c9
SruthiSekkar <sruthisekkar297@gmail.com>
2022-06-30 22:14:48 +0530

    sample

---
+++ .vscode/JavaScript/DOM/DOM-Create/sample27.js
@@ -0,0 +1,28 @@
+var AllCompanies = [
+    {
+        "id": 1,
+        "title": " Wipro Limited ",
+        "image": "https://gromoremoney.in/wp-content/uploads/2021/07/wipro-hacking-eyeopener-for-indian-firms-experts-2019-04-17.jpg"
+
+    },
+    {
+        "id": 2,
+        "title": "Tata Consultancy Services",
+        "image": "https://indiancompanies.in/wp-content/uploads/2020/05/TCS-Logo-Tata-consultancy-service.png",
+    },
+    {
+        "id": 3,
+        "title": "HCL Technologies",
+        "image": "https://www.businessinsider.in/thumb.cms?msid=88944681&width=1200&height=900",
+    },
+    {
+        "id": 4,
+        "title": "Infosys Limited",
+        "image": "https://149695847.v2.pressablecdn.com/wp-content/uploads/2020/05/2020-05-05-scaled.jpg",
+    },
+    {
+        "id": 5,
+        "title": "Amazon.com, Inc",
+        "image": "https://static01.nyt.com/images/2020/06/24/business/24unrest-amazon/00unrest-amazon-mediumSquareAt3X.jpg",
+    },
+]
