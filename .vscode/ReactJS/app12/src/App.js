ea8fbd9ed8065492cbd69ebf049a96bcc230f580
SruthiSekkar <sruthisekkar297@gmail.com>
2022-09-04 21:48:58 +0530

    CRUD

---
+++ .vscode/ReactJS/app12/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { CreateUser } from './CRUD/CreateUser';
+
+function App() {
+  return (
+    <div className="App">
+      <CreateUser />
+    </div>
+  );
+}
+
+export default App;
